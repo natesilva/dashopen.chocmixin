e8484c15ccd6833d37295a458163b5fdc99ecf91
Nate Silva <nate@natesilva.com>
2014-03-06 18:25:57 -0800

    context-sensitive Dash support
    fixes #1

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 This simple Mixin opens the Dash documentation browser and looks up the word currently under the cursor.
 
-After installing, to activate press **Shift-Cmd-H**.
+----
+
+**Note:** The keyboard shortcuts have changed to be more consistent with what is used in other text editors.
+
+----
+
+* **Ctrl-H** will do a context-sensitive search in Dash, automatically selecting the appropriate docset. For example, if you are editing JavaScript code, the JavaScript docset will be searched.
+* **Ctrl-Opt-H** will search all documentation in your current Dash profile.
 
 ## Requirements
 
@@ -15,6 +22,6 @@ From the menu within Chocolat, choose `Actions` > `Install Mixins…`.
 
 ## Usage
 
-Place the cursor over the word to be looked up and press <kbd>Shift</kbd>-<kbd>Cmd</kbd>-<kbd>H</kbd>.
+Place the cursor over the word to be looked up and press <kbd>Ctrl</kbd>-<kbd>H</kbd> for a context-sensitive search, or <kbd>Ctrl</kbd>-<kbd>Opt</kbd>-<kbd>H</kbd> to search all documentation in Dash.
 
-Alternatively, use the menu option `Go` > `Search in Dash`. 
+Alternatively, use the menu option `Go` > `Search in Dash`.
--- init.js
+++ init.js
@@ -1,8 +1,59 @@
-Hooks.addMenuItem('Go/Search in Dash', 'shift-cmd-h', function() {
+/* global Hooks, Recipe, Document */
+
+var path = require('path')
+  , getKeysForFileType = require('./getKeysForFileType.js')
+  , spawn = require('child_process').spawn
+  ;
+
+function openUrl(url) {
+  var child = spawn('open', [url]);
+  var errorText = '';
+  child.stderr.setEncoding('utf8');
+  child.stderr.on('data', function (data) {
+      errorText += data;
+  });
+  child.stderr.on('end', function () {
+      if (errorText.length > 0) {
+          var error = new Error(errorText);
+          throw error;
+      }
+  });
+}
+
+Hooks.addMenuItem('Go/Search in Dash', 'ctrl-alt-h', function() {
   Recipe.run(function(r) {
     var wordrange = r.wordRangeForRange(r.selection);
     var word = r.textInRange(wordrange);
-    var url = 'dash://' + encodeURIComponent(word);
-    require('openurl').open(url);
+    var dashUrl = 'dash://' + encodeURIComponent(word);
+    openUrl(dashUrl);
+  });
+});
+
+Hooks.addMenuItem('Go/Search in Dash (Language Sensitive)', 'ctrl-h', function() {
+  Recipe.run(function(r) {
+    var doc = Document.current();
+
+    var scope = doc.rootScope();
+
+    var fn = doc.filename();
+    var extension = path.extname(fn);
+    if (!extension && fn[0] === '.') { extension = fn; }
+    if (extension && extension[0] === '.') { extension = extension.slice(1); }
+
+    var keys = getKeysForFileType(scope, extension);
+
+    var wordrange = r.wordRangeForRange(r.selection);
+    var word = r.textInRange(wordrange);
+
+    var dashUrl;
+
+    if (keys) {
+      dashUrl = 'dash-plugin://keys=' + keys + '&query=' +
+        encodeURIComponent(word);
+    } else {
+      dashUrl = 'dash://' + encodeURIComponent(word);
+    }
+
+    openUrl(dashUrl);
   });
 });
